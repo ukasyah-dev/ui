0941988de59ade6e5ca76ac53b6f5cd79ad8c9f4
Ukasyah <hi.ukasyah@gmail.com>
2025-05-15 17:32:47 +0700

    update main header

--- src/lib/components/layout/Provider.svelte
+++ src/lib/components/layout/Provider.svelte
@@ -9,6 +9,21 @@
 		href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
 		rel="stylesheet"
 	/>
+
+	<script>
+		if (
+			localStorage.theme === 'dark' ||
+			(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+		) {
+			document.documentElement.setAttribute('data-theme', 'dark');
+			// @ts-ignore
+			window.theme = 'dark';
+		} else {
+			document.documentElement.setAttribute('data-theme', 'light');
+			// @ts-ignore
+			window.theme = 'light';
+		}
+	</script>
 </svelte:head>
 
 {@render children()}
---
+++ src/lib/components/navigation/Button.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { HTMLButtonAttributes } from 'svelte/elements';
+	import { twMerge } from 'tailwind-merge';
+
+	type Props = HTMLButtonAttributes & {
+		square?: boolean;
+		variant?: 'ghost';
+	};
+
+	let { children, class: className, square, variant = 'ghost', ...rest }: Props = $props();
+
+	let _class = twMerge(
+		'cursor-pointer flex items-center justify-center',
+		'transition duration-100',
+		square && 'px-0 w-8 h-8 rounded-lg',
+		variant === 'ghost' && 'hover:bg-background-semimuted',
+		className?.toString()
+	);
+</script>
+
+<button class={_class} {...rest}>
+	{@render children?.()}
+</button>
