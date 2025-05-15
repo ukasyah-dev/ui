fe1dcc3e1673fb368b5eff0cf568a7ca4ae18db0
Ukasyah <hi.ukasyah@gmail.com>
2025-05-15 08:49:19 +0700

    update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import { Container } from '$lib/index.js';
+	import MainHeader from './MainHeader.svelte';
 </script>
 
-<Container class="flex h-full flex-col items-center justify-center">
+<MainHeader />
+
+<Container class="flex flex-1 flex-col items-center justify-center">
 	<h1 class="mb-4 text-3xl font-semibold">Lightweight and modern UI library for Svelte</h1>
 	<p class="mx-auto w-full max-w-2xl text-center text-lg leading-normal">
 		Built to accelerate Svelte app development with ready-to-use, themeable components that let you
---
+++ src/routes/MainHeader.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Container } from '$lib/index.js';
+</script>
+
+<Container class="flex h-16 flex-shrink-0 items-center" size="xl">
+	<p class="mr-auto text-lg font-semibold">
+		<a href="/">ukasyah-dev/ui</a>
+	</p>
+
+	<div class="flex items-center gap-6 font-medium">
+		<a href="/">Getting started</a>
+		<a href="/">Components</a>
+		<a href="/">Examples</a>
+	</div>
+</Container>
